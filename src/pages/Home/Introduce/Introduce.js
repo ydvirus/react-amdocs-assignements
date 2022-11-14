499086ccc419fc580ff1771d71f899227eb04f39
ydvirus <39901608+ydvirus@users.noreply.github.com>
2022-11-14 10:58:26 +0530

    2 files modified

--- src/pages/Home/Introduce/Introduce.js
+++ src/pages/Home/Introduce/Introduce.js
@@ -1,24 +1,24 @@
-import React from "react";
-import introImage from "../../../assets/main-banner-image.png";
-import "./Introduce.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import introImage from '../../../assets/main-banner.svg';
+import './Introduce.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Introduce() {
   return (
     <div className="container introduce-container">
       <div className="row py-5">
         <div className="col-md-6 pt-3">
-          <h1 className="big-green-heading mb-4" >
+          <h1 className="big-green-heading mb-4">
             One-click solution for your static website.
           </h1>
           <p className="mb-4">
-            Image from{" "}
-            <span style={{ textDecoration: "underline" }}>Freepik</span>
+            Image from{' '}
+            <span style={{ textDecoration: 'underline' }}>Freepik</span>
           </p>
           <div className="container">
-            <div className="row" style={{ width: "75%"}}>
+            <div className="row" style={{ width: '75%' }}>
               <div className="col-lg-6 col-md-12 col-12 ps-0">
                 <button className="btn btn-success introduction-success-button mb-4">
                   <FontAwesomeIcon
